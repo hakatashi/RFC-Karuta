c8f7c8ea21d19057686f3157a3cdb16cbe234145
Koki Takahashi <hakatasiloving@gmail.com>
2014-11-19 04:59:20 +0900

    Add animation when button was clicked

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+difficulty = null;
 
+$('.difficulties>button').click(function () {
+	difficulty = Number($(this).attr('data-difficulty'));
+	$('.title-page').transition({
+		scale: 0
+	}, 300, 'easeInBack');
+});
